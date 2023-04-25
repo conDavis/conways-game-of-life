09e85d80b907d6b9153289ae11b06931b914be3f
condavis <davis.con@northeastern.edu>
2023-04-25 17:01:24 -0400

    updating readme and adding cleaner output to classifier agent testing

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 Repository containing an implementation of Conway's game of life and several AI agents attempting to prolong life within the game. 
 
 # Running Instructions
-Open a terminal within the project folder and run `python main.py`.
+- Open a terminal within the project folder and run `python main.py` to compare the performance of our heuristic-based agents over 10 generations.
+
+To run our classifiers:
+- run `python train_and_test_bin_class.py` to see the results from our binary classifier which predicts whether a board will sustain infinite life or not.
+- run `python train_and_test_multi_class.py` to see the results from our multiclass classifier which predicts which lifespan class a board will fall into. 
+
+To run our classifier-based agent:
+- run `python test_agent_informed_by_classifier.py` to see results of our agent's play on different board configurations over 50 generations.
--- test_agent_informed_by_classifier.py
+++ test_agent_informed_by_classifier.py
@@ -4,6 +4,7 @@ from game import Game
 from heuristics import IdealNeighborsHeuristic, MaxNeighborsHeuristic, MinNeighborsHeuristic
 import pickle
 import warnings
+import time
 # to supress warnings related to classifier input not including feature names
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -34,11 +35,30 @@ with open('trained_multi_class_classifier.pkl', 'rb') as f:
     multi_class = pickle.load(f)
 
 multi_class_informed_agent = ClassifierBasedAgent(convert_board_to_multi_class_input, 2, multi_class)
-board = Board(5, 5, {(2, 0), (2, 1), (2, 2), (2, 3)})
-game_with_initially_infinite_config = Game(board.copy(), 50)
 board = Board(5, 5, {(1, 1), (3, 2), (2, 3)})
 game_with_initially_non_infinite_config = Game(board.copy(), 50)
+game_with_initially_non_infinite_config_5_gen = Game(board.copy(), 5)
+board = Board(5, 5, {(2, 0), (2, 1), (2, 2), (2, 3)})
+game_with_initially_infinite_config = Game(board.copy(), 50)
+game_with_initially_infinite_config_5_gen = Game(board.copy(), 5)
 
-
-game_with_initially_non_infinite_config.play_without_agent(with_sleeps=False)
+# testing on an initially non-infinite board
+print('Initially non-infinite configuration without agent interference: ')
+time.sleep(1)
+game_with_initially_non_infinite_config_5_gen.play_without_agent()
+print('Dies out within 5 generations.\n')
+time.sleep(1)
+print('The same initially non-infinite configuration with our agent\'s interference:')
+time.sleep(5)
 game_with_initially_non_infinite_config.play_with_agent(multi_class_informed_agent, with_sleeps=False)
+print('Our agent creates infinite life from an initially non-infinite configuration!')
+print('-----------------------------------------------------------------------------')
+# testing on an initially infinite board
+time.sleep(1)
+print('Initially infinite configuration without agent interference: ')
+time.sleep(1)
+game_with_initially_infinite_config_5_gen.play_without_agent()
+print('The same initially infinite configuration with our agent\'s interference:')
+time.sleep(5)
+game_with_initially_infinite_config.play_with_agent(multi_class_informed_agent, with_sleeps=False)
+print('Our agent sustains infinite life!')
